06a8db90bb804e8de908044c7580ad90864a197c
avierr <kritrim@yandex.com>
2023-02-04 21:24:38 +0000

    format code

--- interpret.js
+++ interpret.js
@@ -1,13 +1,13 @@
 let program = [
-  { def: { 'a': 10 }},
-  { def: { 'b': 20 }},
-  { print: { '>': [{val: 'b'},{val:'a'}]}}, 
-  { print: { '<': [{val: 'a'},{val:'b'}]}},
-  { print: { '==': [{val: 'a'},{val:'b'}]}},
+  { def: { 'a': 10 } },
+  { def: { 'b': 20 } },
+  { print: { '>': [{ val: 'b' }, { val: 'a' }] } },
+  { print: { '<': [{ val: 'a' }, { val: 'b' }] } },
+  { print: { '==': [{ val: 'a' }, { val: 'b' }] } },
   //b > a OR a == b
-  { print:  { '||': [{ '>': [{val: 'b'},{val:'a'}]},{ '==': [{val: 'a'},{val:'b'}]}] } },
+  { print: { '||': [{ '>': [{ val: 'b' }, { val: 'a' }] }, { '==': [{ val: 'a' }, { val: 'b' }] }] } },
   //b > a AND a ==b 
-  { print:  { '&&': [{ '>': [{val: 'b'},{val:'a'}]},{ '==': [{val: 'a'},{val:'b'}]}] } },
+  { print: { '&&': [{ '>': [{ val: 'b' }, { val: 'a' }] }, { '==': [{ val: 'a' }, { val: 'b' }] }] } },
 ]
 
 let builtins = {
@@ -15,51 +15,51 @@ let builtins = {
 }
 
 let binaryOperators = {
-  '+': (a,b) => { return a+b },
-  '-': (a,b) => { return a-b },
-  '*': (a,b) => { return a*b },
-  '>': (a,b) => { return a > b },
-  '<': (a,b) => { return a < b },
-  '==': (a,b) => { return a == b },
-  '||': (a,b) => { return a || b },
-  '&&': (a,b) => { return a && b },
+  '+': (a, b) => { return a + b },
+  '-': (a, b) => { return a - b },
+  '*': (a, b) => { return a * b },
+  '>': (a, b) => { return a > b },
+  '<': (a, b) => { return a < b },
+  '==': (a, b) => { return a == b },
+  '||': (a, b) => { return a || b },
+  '&&': (a, b) => { return a && b },
 }
 
-interpret = (program,state) => {
+interpret = (program, state) => {
   for (let stmt of program) {
-    exec(stmt,state)
+    exec(stmt, state)
   }
 };
 
-varDefinition = (defObj, state) => { 
+varDefinition = (defObj, state) => {
   let variable = Object.keys(defObj)[0];
-  state[variable] = exec(defObj[variable],state)
- }
+  state[variable] = exec(defObj[variable], state)
+}
 
-varValue = (varKey,state) => { return state[varKey]  }
+varValue = (varKey, state) => { return state[varKey] }
 
-exec = (stmt,state) => {
+exec = (stmt, state) => {
 
-  if(typeof stmt === 'number' || typeof stmt === 'string'){
+  if (typeof stmt === 'number' || typeof stmt === 'string') {
     return stmt;
   }
 
   let key = Object.keys(stmt)[0]
   if (typeof builtins[key] === 'function') {
-    builtins[key](exec(stmt[key],state),state)
+    builtins[key](exec(stmt[key], state), state)
   } else if (typeof binaryOperators[key] === 'function') {
     let firstArgument = stmt[key][0];
     let secondArgument = stmt[key][1];
-    return binaryOperators[key](exec(firstArgument,state), exec(secondArgument,state))
-  }else if (key === 'def') {
+    return binaryOperators[key](exec(firstArgument, state), exec(secondArgument, state))
+  } else if (key === 'def') {
     let firstArgument = stmt[key]
     return varDefinition(firstArgument, state)
-  }else if (key === 'val') {
+  } else if (key === 'val') {
     let firstArgument = stmt[key]
     return varValue(firstArgument, state)
-  }else{
-    console.error('unknown instruction: '+key)
+  } else {
+    console.error('unknown instruction: ' + key)
   }
 }
 
-interpret(program,{})
+interpret(program, {})
