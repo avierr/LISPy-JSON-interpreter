3a7c474d2506fcc86be28aeafd39b1f6e8dbce93
avierr <kritrim@yandex.com>
2023-02-04 21:13:53 +0000

    define variables and use their value

--- interpret.js
+++ interpret.js
@@ -1,6 +1,8 @@
 let program = [
+  { def: { 'a': 10 }},
+  { def: { 'b': 20 }},
   { 
-    print: { '+': [3, {'*': [5, 6]}] } 
+    print: { '+': [{ 'val': 'a' }, { 'val': 'b' }] }
   },
 ]
 
@@ -14,13 +16,20 @@ let binaryOperators = {
   '*': (a,b) => { return a*b },
 }
 
-interpret = (program) => {
+interpret = (program,state) => {
   for (let stmt of program) {
-    exec(stmt)
+    exec(stmt,state)
   }
 };
 
-exec = (stmt) => {
+varDefinition = (defObj, state) => { 
+  let variable = Object.keys(defObj)[0];
+  state[variable] = exec(defObj[variable],state)
+ }
+
+varValue = (varKey,state) => { return state[varKey]  }
+
+exec = (stmt,state) => {
 
   if(typeof stmt === 'number' || typeof stmt === 'string'){
     return stmt;
@@ -28,14 +37,20 @@ exec = (stmt) => {
 
   let key = Object.keys(stmt)[0]
   if (typeof builtins[key] === 'function') {
-    builtins[key](exec(stmt[key]))
+    builtins[key](exec(stmt[key],state),state)
   } else if (typeof binaryOperators[key] === 'function') {
     let firstArgument = stmt[key][0];
     let secondArgument = stmt[key][1];
-    return binaryOperators[key](exec(firstArgument), exec(secondArgument))
+    return binaryOperators[key](exec(firstArgument,state), exec(secondArgument,state))
+  }else if (key === 'def') {
+    let firstArgument = stmt[key]
+    return varDefinition(firstArgument, state)
+  }else if (key === 'val') {
+    let firstArgument = stmt[key]
+    return varValue(firstArgument, state)
   }else{
     console.error('unknown instruction: '+key)
   }
 }
 
-interpret(program)
+interpret(program,{})
